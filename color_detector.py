12760686b923f22d545292746716b01275a83e12
vishal-raj15 <vishalraj1380@gmail.com>
2019-10-06 07:37:19 +0530

    commit for color_detector

---
+++ color_detector.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+cap = cv2.VideoCapture(0);
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+
+while True:
+    #frame = cv2.imread('smarties.png')
+    _, frame = cap.read()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    l_b = np.array([110 , 50 ,50])
+    u_b = np.array([130  ,255 , 255])
+
+
+
+    mask = cv2.inRange(hsv, l_b, u_b)
+
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("res", res)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
